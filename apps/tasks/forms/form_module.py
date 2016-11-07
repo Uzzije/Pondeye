6b34edafc48c6791a991dc454ff42f65efa7571e
Uzzi Emuchay <Uzzije2000@yahoo.co.uk>
2016-11-06 22:51:12 -0600

    updated the removal of pillow

--- apps/tasks/forms/form_module.py
+++ apps/tasks/forms/form_module.py
@@ -4,4 +4,5 @@ from django.utils import timezone
 
 def get_current_datetime():
     local = timezone.localtime(timezone.now(), timezone=get_localzone())
+    print "%s local time" % local
     return local
