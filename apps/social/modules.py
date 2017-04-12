4e0d45723f7a4bee72d2250bfaced8149336b831
Uzzi Emuchay <Uzzije2000@yahoo.co.uk>
2017-04-11 20:57:37 -0500

    updated progress

--- apps/social/modules.py
+++ apps/social/modules.py
@@ -332,8 +332,9 @@ def get_progress_set(progress_set, timezone):
     list_progress_entry = []
     for each_set in progress_set:
         set_dic = {'name_of_project':each_set.project.name_of_project, 'id':each_set.id, 'list_of_progress_pictures':[]}
+        print "each prog ", each_set
         for each_progress in each_set.list_of_progress_pictures.all().filter(is_deleted=False):
-            print "each prog ", each_progress
+
             set_dic['list_of_progress_pictures'].append({
                 'progress_message':each_progress.name_of_progress,
                 'date_created': utc_to_local(each_progress.last_updated, local_timezone=timezone).strftime("%B %d %Y %I:%M %p"),
