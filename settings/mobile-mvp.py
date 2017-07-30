a483d13e0f453675893ec94f6c145579bc734e25
Uzzi Emuchay <Uzzije2000@yahoo.co.uk>
2017-07-29 22:24:35 -0700

    Added EB  database config and requirements files

--- settings/mobile-mvp.py
+++ settings/mobile-mvp.py
@@ -12,3 +12,25 @@ DATABASES = {
 CORS_ORIGIN_WHITELIST = (
 	'http://localhost:8100'
 )
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+	DATABASES = {
+		'default':{
+			'ENGINE': 'django.db.backends.mysql',
+			'NAME': 'Uzzije$mobile-data-base',
+			'USER': 'Uzzije',
+			'PASSWORD': DATABASE_PASSWORD,
+			'HOST': 'Uzzije.mysql.pythonanywhere-services.com',
+		}
+	}
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
