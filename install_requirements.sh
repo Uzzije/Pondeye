82ce75698b9cc1e8d74fadffcee138ea9c9fa099
Uzzi Emuchay <Uzzije2000@yahoo.co.uk>
2017-08-13 14:10:21 -0700

    update req

--- install_requirements.sh
+++ install_requirements.sh
@@ -4,12 +4,10 @@
 yum install libjpeg-turbo-devel
 yum install libpng-devel
 yum install freetype-devel
-eb ssh -i i-0ef773441e3e03659
-sudo su -
 cd /usr/local/bin
+export EMAIL_PASSWORD='DaKuimcv1'
 mkdir ffmpeg
-wget ffmpeg.static.64bit.2014-03-02.tar.gz
+wget  http://ffmpeg.gusari.org/static/64bit/ffmpeg.static.64bit.2014-03-02.tar.gz
 tar -xzf ffmpeg.static.64bit.2014-03-02.tar.gz
 ln -s /usr/local/bin/ffmpeg/ffmpeg /usr/bin/ffmpeg
-export FFMPEG_BINARY=/usr/bin/ffmpeg
-
+export FFMPEG_BINARY=/usr/local/bin/ffmpeg/ffmpeg
