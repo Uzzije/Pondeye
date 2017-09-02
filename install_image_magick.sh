98ea37390cfae7830219825186efa3fc34006b2d
Uzzi Emuchay <Uzzije2000@yahoo.co.uk>
2017-09-01 22:02:59 -0500

    Update moviepy

--- install_image_magick.sh
+++ install_image_magick.sh
@@ -14,3 +14,14 @@ ldconfig /usr/local/lib
 convert --version
 ln -s /usr/local/bin/image-magick/convert /usr/bin/convert
 export IMAGEMAGICK_BINARY=/usr/bin/convert
+wget  http://ffmpeg.gusari.org/static/64bit/ffmpeg.static.64bit.2014-03-02.tar.gz
+mv ffmpeg.static.64bit.2014-03-02.tar.gz ffmpeg
+tar -xzf ffmpeg.static.64bit.2014-03-02.tar.gz
+cd ffmpeg
+
+./configure
+make
+make install
+ln -s /usr/local/bin/ffmpeg/ffmpeg /usr/bin/ffmpeg
+export FFMPEG_BINARY=/usr/bin/ffmpeg
+
