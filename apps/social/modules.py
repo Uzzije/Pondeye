6930880216063affd588fd4b2236e8ce2b2d2e00
Uzzi Emuchay <Uzzije2000@yahoo.co.uk>
2017-11-24 17:30:15 -0600

    updated file location

--- apps/social/modules.py
+++ apps/social/modules.py
@@ -1526,8 +1526,12 @@ def make_timeline_video(progress_set):
     final_clips = concatenate_videoclips(video_clips)
     final_clips_name = progress_set.project.name_of_project + randomword(12) + ".mp4"
     abs_path = os.path.join(os.path.abspath('/tmp/'), final_clips_name)
-    final_clips.write_videofile(abs_path, audio=False)
-    f = open(final_clips_name)
-    progress_set.video_timeline.save(final_clips_name, File(f))
+    final_clips.write_videofile(abs_path,
+                                remove_temp=True,
+                                audio_codec='aac',
+                                codec='libx264',
+                                )
+    f = open(abs_path)
+    progress_set.video_timeline.save(abs_path, File(f))
 
 
