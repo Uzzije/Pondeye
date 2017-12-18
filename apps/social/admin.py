62bdc9ed81b1a9fa4a4d34a4219983d14b59352a
Uzzi Emuchay <Uzzije2000@yahoo.co.uk>
2017-12-18 00:11:56 -0600

    added a bunch of model fields

--- apps/social/admin.py
+++ apps/social/admin.py
@@ -1,35 +1,29 @@
 from django.contrib import admin
-from models import Follow, SeenMilestone, ProfilePictures, Notification, PictureSet, VoucheMilestone, \
-	Picture, JournalPost, JournalComment, Pond, PondRequest, PondMembership, PondSpecificProject, SeenProject, \
-	LetDownMilestone, ProgressPicture, ProgressPictureSet, SeenProgress, SeenPictureSet, WorkEthicRank, LetDownProject, \
-	VoucheProject, ProgressImpressedCount, PondProgressFeed, ShoutOutEmailAndNumber, ProgressVideo, ProgressVideoSet
+from models import FollowChallenge, Notification, \
+	Pond, PondRequest, PondMembership, PondSpecificProject,\
+	PondProgressFeed, ShoutOutEmailAndNumber, ProgressVideo, ProgressVideoSet,\
+	SeenChallenge, SeenRecentUpload, SeenVideoSet, CommentRecentUploads, Challenge, ChallengeRating, \
+	ChallengeVideo, CommentChallengeAcceptance, CommentRequestFeed, CommentVideoCelebrations
 
 # Register your models here.
 
-admin.site.register(Follow)
-admin.site.register(SeenMilestone)
-admin.site.register(ProfilePictures)
+admin.site.register(SeenChallenge)
+admin.site.register(SeenRecentUpload)
+admin.site.register(SeenVideoSet)
 admin.site.register(Notification)
-admin.site.register(PictureSet)
-admin.site.register(VoucheMilestone)
-admin.site.register(Picture)
-admin.site.register(JournalPost)
-admin.site.register(JournalComment)
+admin.site.register(CommentRecentUploads)
+admin.site.register(Challenge)
+admin.site.register(ChallengeRating)
+admin.site.register(ChallengeVideo)
+admin.site.register(CommentChallengeAcceptance)
 admin.site.register(Pond)
 admin.site.register(PondRequest)
 admin.site.register(PondMembership)
 admin.site.register(PondSpecificProject)
-admin.site.register(SeenProject)
-admin.site.register(LetDownMilestone)
-admin.site.register(ProgressPicture)
-admin.site.register(ProgressPictureSet)
-admin.site.register(SeenPictureSet)
-admin.site.register(SeenProgress)
-admin.site.register(LetDownProject)
-admin.site.register(WorkEthicRank)
-admin.site.register(VoucheProject)
-admin.site.register(ProgressImpressedCount)
+admin.site.register(CommentRequestFeed)
+admin.site.register(CommentVideoCelebrations)
 admin.site.register(PondProgressFeed)
 admin.site.register(ShoutOutEmailAndNumber)
 admin.site.register(ProgressVideo)
 admin.site.register(ProgressVideoSet)
+admin.site.register(FollowChallenge)
