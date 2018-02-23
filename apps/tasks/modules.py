d2ea5f745db85e59ba31679f26946f3b9c926416
Uzzi Emuchay <Uzzije2000@yahoo.co.uk>
2018-02-23 12:09:49 -0600

    update tasks.py url

--- apps/tasks/modules.py
+++ apps/tasks/modules.py
@@ -879,9 +879,8 @@ def get_challenge_request(user, local_timezone='UTC'):
         ch_req['created'] = utc_to_local(each_c.created, local_timezone=local_timezone).strftime("%B %d %Y %I:%M %p")
         ch_req['challenger_id'] = each_c.challenger.user.id
         ch_list.append(ch_req)
-    sorted(ch_list, key=lambda x: x['dt_created'], reverse=True)
-
-    return ch_list
+    ch_list_sorted = sorted(ch_list, key=lambda x: x['dt_created'], reverse=True)
+    return ch_list_sorted
 
 
 def randomword(length):
